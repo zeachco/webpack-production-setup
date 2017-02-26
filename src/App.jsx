41dcd446efd00aca12787df51c905bbde16be0bc
Olivier Rousseau-Guyot <zeachco@gmail.com>
2017-02-26 18:52:04 -0500

    style

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,12 @@ import React from 'react';
 const App = () => (
 	<div className="jumbotron">
 		<h1>Awesome!</h1>
-			<p>This project comes with a modifiable version of <a href="https://v4-alpha.getbootstrap.com/">Bootstrap 4</a> css framework</p>
-			<p>It supports hot-loading for both CSS and JS transpilations</p>
+		<p>
+			<ol>
+				<li>This project comes with a modifiable version of <a href="https://v4-alpha.getbootstrap.com/">Bootstrap 4</a> css framework</li>
+				<li>It supports hot-loading for both CSS and JS transpilations</li>
+			</ol>
+		</p>
 	</div>
 );
 
