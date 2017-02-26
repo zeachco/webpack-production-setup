582311fed3a0524eb4e29903332bb96c54ae2058
Olivier Rousseau-Guyot <zeachco@gmail.com>
2017-02-26 18:47:49 -0500

    bootstrap sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import 'styles';
+import 'styles/theme.scss';
 import React from 'react';
 
-const App = () => (<div>
-	<h1>Awesome!</h1>
-	<p>you you have a project which supports hot-loading</p>
-</div>);
+const App = () => (
+	<div className="jumbotron">
+		<h1>Awesome!</h1>
+			<p>This project comes with a modifiable version of <a href="https://v4-alpha.getbootstrap.com/">Bootstrap 4</a> css framework</p>
+			<p>It supports hot-loading for both CSS and JS transpilations</p>
+	</div>
+);
 
 export default App;
