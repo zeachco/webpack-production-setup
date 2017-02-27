1a17d0f7caa606179f0c75726539b9ecfe9ab33b
Olivier Rousseau-Guyot <zeachco@gmail.com>
2017-02-26 23:11:07 -0500

    correct dom syntax

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,10 @@ import React from 'react';
 const App = () => (
 	<div className="jumbotron">
 		<h1>Awesome!</h1>
-		<p>
-			<ol>
-				<li>This project comes with a modifiable version of <a href="https://v4-alpha.getbootstrap.com/">Bootstrap 4</a> css framework</li>
-				<li>It supports hot-loading for both CSS and JS transpilations</li>
-			</ol>
-		</p>
+		<ol>
+			<li>This project comes with a modifiable version of <a href="https://v4-alpha.getbootstrap.com/">Bootstrap 4</a> css framework</li>
+			<li>It supports hot-loading for both CSS and JS transpilations</li>
+		</ol>
 	</div>
 );
 
