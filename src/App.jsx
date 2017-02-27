19932496e9d7ce991ae6a58a69915989ca5d7a1d
Olivier Rousseau-Guyot <zeachco@gmail.com>
2017-02-27 08:38:04 -0500

    text

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,10 @@ const App = () => (
 		<ol>
 			<li>This project comes with a modifiable version of <a href="https://v4-alpha.getbootstrap.com/">Bootstrap 4</a> css framework</li>
 			<li>It supports hot-loading for both CSS and JS transpilations</li>
+			<li>It have working sourcemaps for both babel js and sass files</li>
+			<li>have an appcache that allows offline access to the app</li>
 		</ol>
-		<button className="btn btn-primary">A button</button>
+		<button className="btn btn-primary">A Button</button>
 		<button className="btn btn-secondary">Another Button</button>
 	</div>
 );
