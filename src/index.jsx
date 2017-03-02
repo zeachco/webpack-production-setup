b5573f4825a4663a9d16f296fae1a04be982840c
Olivier Rousseau-Guyot <zeachco@gmail.com>
2017-03-02 07:52:08 -0500

    cleaner hook

--- src/index.jsx
+++ src/index.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import App from './containers/App';
-import store from './store';
 import { Provider } from 'react-redux';
 
-const Hook = (Component) => render(
+const Hook = () => {
+	const App = require('./containers/App').default;
+	const store = require('./store').default;
+	render(
 	<AppContainer>
 		<Provider store={store}>
-			<Component />
+			<App />
 		</Provider>
 	</AppContainer>,
-	document.querySelector("#root"));
+	document.querySelector("#root"));}
 
-Hook(App);
+Hook();
 
 // HRM functionality
 if (module && module.hot) {
-  module.hot.accept('./containers/App', () => {
-    console.clear(); // eslint-disable-line no-console
-		const HotApp = require('./containers/App').default;
-		Hook(HotApp);
-	});
+  module.hot.accept('./containers/App', Hook);
+  module.hot.accept('./store', Hook);
 }
