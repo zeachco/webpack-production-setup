1901e78d3e76fbb248b5c5d214ef6b448c5c7b4d
Olivier Rousseau-Guyot <zeachco@gmail.com>
2017-03-02 08:07:22 -0500

    counter disabled and head script rename

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,9 +4,9 @@ import { actions } from '../store';
 
 const Counter = ({count, increase, decrease}) => (
 	<div className="btn-group" role="group" aria-label="Basic example">
-		<button type="button" className="btn btn-secondary" onClick={decrease}>Substract</button>
-		<button type="button" className="btn" disabled>{count}</button>
-		<button type="button" className="btn btn-primary" onClick={increase}>Increment</button>
+		<button className="btn btn-secondary" disabled={ count < 1 } onClick={decrease}>Substract</button>
+		<button className="btn" disabled>{count}</button>
+		<button className="btn btn-primary" disabled={ count > 14 }onClick={increase}>Increment</button>
 	</div>
 );
 
